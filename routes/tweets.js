62cc371a8562ce219e29e069d9ab92a6cbe3d3d2
Caden Sumner <cadenjsumner@gmail.com>
2018-02-24 15:36:40 -0500

    Tweet stuff

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var pubnub = require('pubnub');
 var stream = require('pubnub-twitter/pubnub-twitter');
 /* GET tweets */
 router.get('/', function (req, res, next) {
     var tweets = [];
     stream(function (tweet) {
+        if(tweet["geo"]){
             tweets.push(tweet);
+            }
     });
     setTimeout((function () {
         res.send(tweets);
